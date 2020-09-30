4fd4a9bf7f1daa2030aeb6baa8aafc57d9f84bdb
Mushi2216 <thomahaine@gmail.com>
2020-09-30 18:23:33 +0200

    server and env ok

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+//load env
+dotenv.config({ path: './config.env' });
+
+
+const app = express();
+
+app.get('/api/v1/profile/:platform/:gamertag', (req,res) => {
+    console.log(req.params.platform, req.params.gamertag);
+    res.send('Hello');
+});
+const port = process.env.PORT;
+
+app.listen(port, () => {
+ console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+});
+
