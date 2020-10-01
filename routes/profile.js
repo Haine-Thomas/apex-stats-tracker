9dd341808277a6cdf6bfaf42d15705179252e636
Mushi2216 <thomahaine@gmail.com>
2020-10-01 14:43:51 +0200

    adding a 404 if profile not found ok

--- routes/profile.js
+++ routes/profile.js
@@ -12,8 +12,14 @@ router.get('/:platform/:gamertag', async (req,res) => {
 
        const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, {
            headers
-       });
-    const data = await response.json();
+        });
+        const data = await response.json();
+
+        if(data.errors && data.errors.length > 0) {
+            return res.status(404).json({
+                message:'Profile Not Found'
+            })
+        };
     res.json(data);
    } catch (error) {
        console.error(error);
